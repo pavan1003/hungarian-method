30f43dfec2e6b7a45845d84773f62df18acb1309
Pavan Mistry <71229066+pavan1003@users.noreply.github.com>
2022-10-23 14:06:08 +0530

    Add files via upload

--- hungarianmethod.py
+++ hungarianmethod.py
@@ -1,5 +1,4 @@
-from cmath import inf, nan
-from itertools import count
+from cmath import inf
 import numpy as np
 
 # def printMatrix(mat):
@@ -64,11 +63,11 @@ import numpy as np
 #     [2, 0, 5, 1, 0],
 # ]
 
-# matrix=[
-#     [10,12,19,11],
-#     [5,10,7,8],
-#     [12,14,13,11],
-#     [8,15,11,9]
+# matrix = [
+#     [10, 12, 19, 11],
+#     [5, 10, 7, 8],
+#     [12, 14, 13, 11],
+#     [8, 15, 11, 9]
 # ]
 
 matrix=[
@@ -77,7 +76,6 @@ matrix=[
     [4,6,5,3],
     ['M',2,7,3]
 ]
-
 
 
 global Rows, Columns
@@ -117,7 +115,7 @@ def zeroColumn(zeroColumnMatrix):
 def tickRowCol(tickMatrix):
 
     unticked_R = []
-    ticked_R =[]
+    ticked_R = []
     ticked_C = []
     # print(ticked_R,ticked_C,unticked_R)
     # print(tickMatrix)
@@ -126,46 +124,51 @@ def tickRowCol(tickMatrix):
         # print(np.count_nonzero(tickMatrix[i]==0))
         if (np.count_nonzero(tickMatrix[i] == 0) == 0):
             for j in range(np.count_nonzero(np.isnan(tickMatrix[i]))):
-                ticked_C.append(np.argwhere(np.isnan(tickMatrix[i])).flatten()[j])
+                ticked_C.append(np.argwhere(
+                    np.isnan(tickMatrix[i])).flatten()[j])
             ticked_R.append(i)
-            pos_nan=np.argwhere(np.isnan(tickMatrix[i]))
+            pos_nan = np.argwhere(np.isnan(tickMatrix[i]))
             # print(pos_nan)
             for j in range(len(pos_nan)):
                 pos_0 = np.argwhere(tickMatrix[:, pos_nan[j][0]] == 0)
                 ticked_R.append(pos_0.flatten()[0])
                 if (np.isnan(tickMatrix[pos_0][0][0]).any()):
                     # ticked_R.append(np.argwhere(tickMatrix[:, np.argwhere(np.isnan(tickMatrix[pos_0[0][0]]))[0][0]] == 0)[0][0])
-                    ticked_C.append(np.argwhere(np.isnan(tickMatrix[pos_0[0][0]]))[0][0])
-    # print(pos_nan)
-    ticked_R = np.unique(ticked_R)
-    unticked_R = np.unique(np.delete(unticked_R, np.where(np.in1d(unticked_R, ticked_R))))
-    ticked_C = np.unique(ticked_C)
-
-    tickMatrix[np.isnan(tickMatrix)] = 0
-    minimum_of_cut=inf
-    for i in range(Rows):
-        for j in range(Columns):
-            if i in ticked_R and j not in ticked_C:
-                if minimum_of_cut>tickMatrix[i][j]:
-                    minimum_of_cut=tickMatrix[i][j]
-    for i in range(Rows):
-        for j in range(Columns):
-            if i in ticked_R and j not in ticked_C:
-                tickMatrix[i][j]=tickMatrix[i][j]-minimum_of_cut
+                    ticked_C.append(np.argwhere(
+                        np.isnan(tickMatrix[pos_0[0][0]]))[0][0])
     
+    ticked_R = np.unique(ticked_R)
+    unticked_R = np.unique(
+        np.delete(unticked_R, np.where(np.in1d(unticked_R, ticked_R))))
+    ticked_C = np.unique(ticked_C)
+    # print("Ticks:",ticked_R,ticked_C,unticked_R)
+    
+    tickMatrix[np.isnan(tickMatrix)] = 0
+    minimum_of_cut = inf
+    for i in range(Rows):
+        for j in range(Columns):
+            if i in ticked_R and j not in ticked_C:
+                if minimum_of_cut > tickMatrix[i][j]:
+                    minimum_of_cut = tickMatrix[i][j]
+    for i in range(Rows):
+        for j in range(Columns):
+            if i in ticked_R and j not in ticked_C:
+                tickMatrix[i][j] = tickMatrix[i][j]-minimum_of_cut
+
     for i in unticked_R:
         for j in ticked_C:
-            tickMatrix[i][j]=tickMatrix[i][j]+minimum_of_cut
+            tickMatrix[i][j] = tickMatrix[i][j]+minimum_of_cut
 
-    count_assignment,tickMatrix = assignMatrix(tickMatrix)
+    count_assignment, tickMatrix = assignMatrix(tickMatrix)
 
     # print(ticked_R,ticked_C,unticked_R,minimum_of_cut)
     return tickMatrix
 
+
 def assignMatrix(assignMat):
 
     assignMat = assignMat.astype('float')
-    
+
     for i in range(Rows):
 
         if (np.count_nonzero(assignMat[i] == 0) == 1):
@@ -185,52 +188,45 @@ def assignMatrix(assignMat):
                 if (assignMat[pos, j] == 0):
                     assignMat[pos, j] = np.nan
             assignMat[pos, i] = 0
-    
-    count_assignment=np.count_nonzero(assignMat == 0)
+
+    count_assignment = np.count_nonzero(assignMat == 0)
 
     assignMatrix.counter += 1
 
-    if(assignMatrix.counter>30):
-        for i in range(Rows):
-            if(i<Rows-1):
-                if (np.count_nonzero(assignMat[i] == 0) >= 1):
-                    pos = np.argwhere(assignMat[i] == 0)
-                    for j in range(len(pos)):
-                        if (assignMat[i][pos[j][0]] == 0 and assignMat[i+1][pos[j][0]] == 0):
-                            continue
-                        else:
-                            for k in range(Columns):
-                                if (assignMat[k, pos[j][0]] == 0):
-                                    assignMat[k, pos[j][0]] = np.nan
-                                assignMat[i, pos[j][0]] = 0 
-            else:
-                if (np.count_nonzero(assignMat[i] == 0) >= 1):
-                    pos = np.argwhere(assignMat[i] == 0)
-                    for j in range(len(pos)-1):
-                        if(matClone[i][pos[j][0]]>matClone[i][pos[j+1][0]]):
-                            assignMat[i, pos[j][0]]=np.nan
-                        else:
-                            assignMat[i, pos[j+1][0]]=np.nan
+    if (assignMatrix.counter > 20):
+        assignMat[np.isnan(assignMat)] = 0
+        from munkres import Munkres 
+        mun= Munkres()
+        mat=mun.compute(assignMat)
+        assignMat[assignMat == 0 ] = np.nan
+        for i in range(len(mat)):
+            assignMat[mat[i][0]][mat[i][1]]=0
+        # print(mat)
+        # print(assignMat)
 
-        for i in range(Columns):
-            if (np.count_nonzero(assignMat[:, i] == 0) == 1):
-                pos = np.argwhere(assignMat[:, i] == 0)
-                for j in range(Rows):
-                    if (assignMat[pos, j] == 0):
-                        assignMat[pos, j] = np.nan
-                assignMat[pos, i] = 0
-
-    elif(count_assignment!=Rows):
-        assignMat=tickRowCol(assignMat)
+    elif (count_assignment != Rows):
+        assignMat = tickRowCol(assignMat)
+    # print(assignMat)
 
     return count_assignment, assignMat
 
 
-def jobCostCal(jobMatrix,matrixClone):
-    matrixClone=np.asarray(matrixClone)
+def Min_To_Max(maxMatrix):
+    maxvalue=(maxMatrix[maxMatrix != np.inf]).max()
+    # print("maxvalue: ",maxvalue)
+    maxMatrix=maxvalue-maxMatrix
+    if(maxMatrix[maxMatrix==-inf].size>0):
+        maxMatrix[maxMatrix==-inf]=inf
+    return (maxMatrix)
+
+
+def jobCostCal(jobMatrix, matrixClone):
+    matrixClone = np.asarray(matrixClone)
     pos = np.argwhere(jobMatrix == 0)
     # print(matrixClone)
-    # print(pos)
+    print("\nAssignment Position:")
+    for i in range(Rows):
+        print(pos[i],"-->",matrixClone[pos[i][0]][pos[i][1]])
     cost = []
     for i in range(Rows):
         # row = pos[i]
@@ -244,15 +240,15 @@ Rows = len(matrix)
 Columns = len(matrix[0])
 matrix = np.reshape(matrix, (Rows, Columns))
 
-if (type(matrix[0][0])==np.str_):
+if (type(matrix[0][0]) == np.str_):
     for i in range(Rows):
         for j in range(Columns):
             try:
                 matrix[i][j].astype('float')
             except:
-                matrix[i][j]=inf
+                matrix[i][j] = inf
 
-    matrix=matrix.astype('float')
+    matrix = matrix.astype('float')
 
 matrix = dummyRowCol(matrix)
 Rows = len(matrix)
@@ -261,25 +257,39 @@ Columns = len(matrix[0])
 # matClone = matrix
 matClone = tuple(map(tuple, matrix))
 # matClone = matClone.astype('float')
-print("Matrix After Adding Dummy Row or Column:\n", matrix)
-# print("Clone:\n",matClone)
+# flag=1
+while True:
+    Max_Min = input("1. Maximize \n2. Minimize \nEnter Choice: ")
+    print(Max_Min)
+    if (Max_Min == '1' or Max_Min == '2'):
+        break
+    else:
+        print("Wrong Choice! please choose 1 or 2\n")
+        continue
+
+if Max_Min == '1':
+    matrix = Min_To_Max(matrix)
+    print("\nMaximize Matrix:\n", matrix)
+
+if (Columns != Rows):
+    print("\nMatrix After Adding Dummy Row or Column:\n", matrix)
 
 matrix = zeroRow(matrix)
-print("Matrix After subtracting minimum of row from each element of that row:\n", matrix)
+print("\nMatrix After subtracting minimum of row from each element of that row:\n", matrix)
 
 matrix = zeroColumn(matrix)
-print("Matrix After subtracting minimum of column from each element of that column:\n", matrix)
+print("\nMatrix After subtracting minimum of column from each element of that column:\n", matrix)
 
-assignMatrix.counter=0
+assignMatrix.counter = 0
 
-count_assignment,matrix = assignMatrix(matrix)
-if(count_assignment!=Rows):
-    matrix=tickRowCol(matrix)
+count_assignment, matrix = assignMatrix(matrix)
+if (count_assignment != Rows):
+    matrix = tickRowCol(matrix)
 
 # matrix=tickRowCol(matrix)
 
-print("Final Solution:\n", matrix)
+print("\nFinal Solution:\n", matrix)
 
 # print("Clone1:\n",matClone)
-jobCost = jobCostCal(matrix,matClone)
-print("Job cost :\n", jobCost)
+jobCost = jobCostCal(matrix, matClone)
+print("\nJob cost :\n", jobCost)
